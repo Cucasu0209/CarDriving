47375d505defedfe4984ef67d5f4ac92e9a4618c
trungnq10 <>
2025-05-06 09:35:40 +0700

    Update Car

--- Assets/1.Scripts/CarController/Car_Camera.cs
+++ Assets/1.Scripts/CarController/Car_Camera.cs
@@ -6,15 +6,14 @@ using UnityEngine;
 [RequireComponent(typeof(Car_Movement))]
 public class Car_Camera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] private float Height = 20;
+    [SerializeField] private float Distance = 15;
+    [SerializeField] private float Angle = 45;
+    [SerializeField] private float Damping = 5.0f;
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
-
+        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, transform.position + new Vector3(0, Height, -Distance), Time.deltaTime * Damping);
+        Camera.main.transform.rotation = Quaternion.Euler(Angle, 0, 0);
     }
 }
---
+++ Assets/1.Scripts/CarController/Car_Gravity.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Car_Movement))]
+public class Car_Gravity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
